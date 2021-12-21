0e474ee27afd2605e2f71f96f34d57d10d245a1f
Lucas <lucas@POSPEL241.local>
2021-12-21 10:59:24 -0300

    feature: FormField added

---
+++ src/components/FormField/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+  Controller,
+  ControllerProps,
+  FieldValues,
+  Path,
+} from 'react-hook-form';
+
+import TextField from '../TextField';
+
+interface FormFieldProps<T> extends Partial<ControllerProps<T>> {
+  hiddenLabel?: boolean;
+  label?: string;
+  placeholder?: string;
+  name: Path<T>;
+}
+
+const FormField = <T extends FieldValues>({
+  name,
+  control,
+  label,
+  hiddenLabel,
+}: FormFieldProps<T>) => (
+  <Controller
+    name={name}
+    control={control}
+    render={({ field: { onChange, value } }) => (
+      <TextField
+        label={label}
+        {...(hiddenLabel && { hiddenLabel: true })}
+        onChange={onChange}
+        value={value}
+      />
+    )}
+  />
+);
+
+export default FormField;
