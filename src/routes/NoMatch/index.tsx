5d132dc24f4c07cddad53979ede928da55d9cc62
Lucas <lucas@POSPEL241.local>
2021-12-22 19:54:59 -0300

    feature: styles for 404 page added

--- src/routes/NoMatch/index.tsx
+++ src/routes/NoMatch/index.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Box, Typography } from '@mui/material';
 
 const NoMatch: React.FC = () => {
-  return <div>Page Not Found</div>;
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        height: 'calc(100vh - 82px)',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Typography sx={{ fontWeight: 700, fontSize: '70px' }}>404</Typography>
+      <Typography sx={{ fontSize: '20px' }}>Page Not Found</Typography>
+    </Box>
+  );
 };
 
 export default NoMatch;
